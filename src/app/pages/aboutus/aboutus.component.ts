b3b3297701de2ca43a7008c7fc2d9cf1f355718c
Faizan Shaikh <faizan.shaikh@apsissolutions.com>
2023-06-12 10:55:29 +0530

    footer fixed

--- src/app/pages/aboutus/aboutus.component.ts
+++ src/app/pages/aboutus/aboutus.component.ts
@@ -9,6 +9,13 @@ export class AboutusComponent implements OnInit {
 
   constructor() { }
 
+  openPdf(val:number){
+    switch(val){
+      case 1:
+        window.open("src/assets/pdfs/UDI-Exp.Certificate_1993 To 2008.pdf", '_blank');
+        break
+    }
+  }
   ngOnInit(): void {
   }
 
